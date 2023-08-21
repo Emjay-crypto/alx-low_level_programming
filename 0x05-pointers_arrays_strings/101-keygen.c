589b59c3eaa8dd3bd56a093629f53e151d710215
Dennis <dennisukaefi82@gmail.com>
2023-08-21 03:09:16 -0700

    Pointers and array

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,15 +6,15 @@
 
 int main() {
     time_t t;
-    char password[PASSWORD_LENGTH + 1]; // +1 for null-terminator
+    char password[PASSWORD_LENGTH + 1]; /* +1 for null-terminator */
     int i;
 
-    srand((unsigned) time(&t)); // Seed the random number generator with current time
+    srand((unsigned) time(&t)); /* Seed the random number generator with current time */
 
     for (i = 0; i < PASSWORD_LENGTH; i++) {
-        password[i] = rand() % 94 + 33; // Range: 33 ('!') to 126 ('~')
+        password[i] = rand() % 94 + 33; /* Range: 33 ('!') to 126 ('~') */
     }
-    password[PASSWORD_LENGTH] = '\0'; // Null-terminate the string
+    password[PASSWORD_LENGTH] = '\0'; /* Null-terminate the string */
 
     printf("Generated password: %s\n", password);
 
