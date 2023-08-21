2e5ab248981b97b17800a2578f1018e5aca96f4b
Dennis <dennisukaefi82@gmail.com>
2023-08-21 02:44:29 -0700

    Pointers and array

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@
 
 int _atoi(char *s)
 {
-int sign = 1, resp = 0, firstNum;
+int sign = 1, resp = 0, firstNum, i;
 
 for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
 {
@@ -17,12 +17,12 @@ if (s[firstNum] == '-')
 sign *= -1;
 }
 }
-int i;
+
 for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
 {
 resp *= 10;
 resp += (s[i] - 48);
 }
 
-return (sign *resp);
+return (sign * resp);
 }
