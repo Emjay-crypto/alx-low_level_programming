0e2d5c9bc7aed27dbb536cae5ab5ea93d3bc16ff
Dennis <dennisukaefi82@gmail.com>
2023-08-23 16:07:23 -0700

    more pointers and arrays

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,23 +8,33 @@
  * Return: void
  */
 
-void print_buffer(char *b, int size)
-{
-	int i;
+void print_line(char *b, int len, int offset) {
+    int i;
 
-	for (i = 0; i <= (size - 1) / 10 && size; i++)
-	{
-		printf("%08x:", i * 10);
-		if (i < size / 10)
-		{
-			print_line(b, 9, i);
-		}
-		else
-		{
-			print_line(b, size % 10 - 1, i);
-		}
-		putchar("\n");
-	}
-	if (size == 0)
-		putchar("\n");
+    for (i = 0; i < len; i++) {
+        printf("%02x", b[offset * 10 + i]);
+        if (i % 2 == 1)
+            putchar(' ');
+    }
+    for (; i < 10; i++) {
+        printf("  ");
+        if (i % 2 == 1)
+            putchar(' ');
+    }
+}
+
+void print_buffer(char *b, int size) {
+    int i;
+
+    for (i = 0; i < (size + 9) / 10; i++) {
+        printf("%08x:", i * 10);
+        if (i < size / 10) {
+            print_line(b, 10, i);
+        } else {
+            print_line(b, size % 10, i);
+        }
+        putchar('\n');
+    }
+    if (size == 0)
+        putchar('\n');
 }
